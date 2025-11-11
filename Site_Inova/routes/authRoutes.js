d8777e978ec05f0a2fd79a951fad98f78a08c9c9
João Pedro dos Santos Souza <joaoopedrosantos003@gmail.com>
2025-11-11 10:54:25 -0300

    Politica Privacidade

--- Site_Inova/routes/authRoutes.js
+++ Site_Inova/routes/authRoutes.js
@@ -3,6 +3,13 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+router.get('/politica-privacidade', (req, res) => {
+    res.render('public/politica-privacidade', { 
+        layout: 'public', // Especifica o novo layout 'public.handlebars'
+        title: 'Política de Privacidade' // Título da página
+    });
+});
+
 // Se sessionMiddleware já é global no index.js, pode não ser necessário aqui.
 // Mas se for específico para rotas de autenticação, mantenha.
 router.use(authController.sessionMiddleware);
